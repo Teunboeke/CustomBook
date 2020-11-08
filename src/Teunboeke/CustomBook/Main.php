c03ddd8cd3390ee4c4aeba675de0bff1aa3d0f75
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-11-08 12:01:30 +0100

    Update Main.php

--- src/Teunboeke/CustomBook/Main.php
+++ src/Teunboeke/CustomBook/Main.php
@@ -2,4 +2,15 @@
 
 declare(strict_types=1);
 
-namespace Teunboeke/Custombook;
+namespace Teunboeke\Custombook;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\utils\Config;
+use pocketmine\Item\Item;
+use pocketmine\Item\ItemFactory;
+use pocketmine\Item\ItemIds;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
